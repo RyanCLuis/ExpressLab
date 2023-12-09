f8e8c5e4c05c8fb60874f0aef6a1c1185d5bb48f
Ryan Luis <RyanCLuis@gmail.com>
2023-12-09 10:11:13 -0800

    adds get home page

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+// load express
 const express = require('express')
 
+// create our express app
 const app = express()
 
+// Config the app (app.set)
+
+// mount middleware (app.use)
+
+// mount routes
+
+// define "root" in app
 app.get('/', function (req, res) {
-    res.send('<h1>Hello World!</h1>')
+    res.send('<h1>Hello Students</h1>')
+})
+
+app.get('/home', function (req, res) {
+    res.send('<h1>Home Page</h1>')
 })
 
 app.listen(1212, function () {
