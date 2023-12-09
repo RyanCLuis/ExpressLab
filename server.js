5aa98700add317ef026e628234cffc828b74d71f
Ryan Luis <RyanCLuis@gmail.com>
2023-12-09 10:01:23 -0800

    sets up framework and tests page

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.listen(1212, function () {
+    console.log('listening on port 1212')
+})
